0799b91f82eb8c1ff67567167129d4134fa7030e
Shawn <sgiovanni1@yahoo.com>
2023-01-29 21:46:25 -0500

    working code

--- js/main.js
+++ js/main.js
@@ -1,29 +1,53 @@
-const wApp = document.getElementById('weather-container')
-const wForm = document.getElementById('iDform')
-const wSearch = document.getElementById('iDsearch')
-console.log(wApp)
+
+var button = document.querySelector('.button')
+var inputValue = document.querySelector('.inputValue')
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+var tempMin = document.querySelector('.tempMin');
+var tempMax = document.querySelector('.tempMax');
+var tempFeel = document.querySelector('.tempFeel');
+var wind = document.querySelector('.wind');
+var pressure = document.querySelector('.pressure');
+
 // fetch
-fetch(`https://api.openweathermap.org/data/2.5/weather?zip=07305,us&appid=ccf252913750bb60776aac7b2df7cc68`)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data.weather[0].description, data.main.temp, data.main.feels_like, data.wind.speed)
-        for (const cWeather of data) {
-            addWeather(cWeather.weather[0].description, cWeather.main.temp, cWeather.main.feels_like, cWeather.wind.speed)
-        }
-        console.log(data.weather[0].description, cWeather.main.temp, cWeather.main.feels_like, cWeather.wind.speed)
-    })
-    console.log(data.weather[0].description, cWeather.main.temp, cWeather.main.feels_like, cWeather.wind.speed)
-    function addWeather(description, temp, feels_like, speed) {
-        const weatherEl = document.createElement('div')
-        weatherEl.classList.add('card')
 
-        weatherEl.innerHTML = `
-        <div class = 'card-body'>
-            <h2>${description}</h2>
-            <h2>${temp}</h2>
-            <h2>${feels_like}</h2>
-            <h2>${speed}</h2>
-        </div>
-        `
+// fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=ccf252913750bb60776aac7b2df7cc68')
+// .then(response => response.json())   
+// .then(data => console.log(data))   
 
-    }
+// .catch (err => alert('Wrong City Name!'))
+
+
+
+button.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=ccf252913750bb60776aac7b2df7cc68')
+        .then(response => response.json())   
+        .then(data => {
+            var nameValue = data['name'];
+            var descValue = data['weather'][0]['description'];
+            var tempValue = data['main']['temp'];
+            var tempMinValue = data['main']['temp_min'];
+            var tempMaxValue = data['main']['temp_max'];
+            var tempFeelValue = data['main']['feels_like'];
+            var windValue = data['wind']['speed'];
+            var pressureValue = data ['main']['pressure'];
+            
+            name.innerHTML = nameValue;
+            desc.innerHTML = descValue;
+            temp.innerHTML = tempValue;
+            tempMin.innerHTML = tempMinValue;
+            tempMax.innerHTML = tempMaxValue;
+            tempFeel.innerHTML = tempFeelValue;
+            wind.innerHTML = windValue;
+            pressure.innerHTML = pressureValue;
+            
+
+        })
+        
+        
+
+    .catch (err => alert('Wrong City Name!'))
+})
+
+
