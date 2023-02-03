21636fbeb9254a7e19426e84a0f782f6bcab1097
Shawn <sgiovanni1@yahoo.com>
2023-02-03 02:24:49 -0500

    temps are converted

--- js/main.js
+++ js/main.js
@@ -11,11 +11,14 @@ var wind = document.querySelector('.wind');
 var pressure = document.querySelector('.pressure');
 
 
-
+// function TempConverter(valNum){
+//     valNum = parseFloat(valNum);
+//     document.getElementsByClassName("temp").innerHTML=(valNum-32)/1.8;
+// }
 
 
 button.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=ccf252913750bb60776aac7b2df7cc68')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&appid=ccf252913750bb60776aac7b2df7cc68' )
         .then(response => response.json())   
         .then(data => {
             var nameValue = data['name'];
@@ -25,17 +28,17 @@ button.addEventListener('click', function(){
             var tempMaxValue = data['main']['temp_max'];
             var tempFeelValue = data['main']['feels_like'];
             var windValue = data['wind']['speed'];
-            var pressureValue = data ['main']['pressure'];
+            // var humidityValue = data ['main']['humidity'];
             
-            
-            desc.innerHTML = "Sky - "+descValue;
-            temp.innerHTML = "Temp - "+tempValue;
+            name.innerHTML = "City - "+nameValue
+            desc.innerHTML = "Forcast - "+descValue;
+            temp.innerHTML = "Current Temp - "+tempValue;
             tempMin.innerHTML = "Low - "+tempMinValue;
             tempMax.innerHTML = "High - "+tempMaxValue;
-            tempFeel.innerHTML = "Chill - "+tempFeelValue;
+            tempFeel.innerHTML = "Wind Chill - "+tempFeelValue;
             wind.innerHTML = "Wind - "+windValue;
-            pressure.innerHTML = "Pressure - "+pressureValue;
-            
+            // humidity.innerHTML = "Humidity - "+humidityValue;
+
 
         })
         
